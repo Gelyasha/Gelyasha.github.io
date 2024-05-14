6d275f422ef848013d13701c138d0bf90d4c3d8f
Gelyasha <angelina_cukanova@mail.ru>
2024-05-15 01:48:25 +0300

    update gendorize ts

--- Gendorize_TS/error.ts
+++
@@ -1,5 +0,0 @@
-export const ERROR = {
-    INVALID_SEARCH_QUERY: 'The search query must not contain Cyrillic alphabet or numbers',
-    EMPTY_SEARCH_QUERY: 'The search query must not be empty',
-    NAME_NOT_FOUND: 'Name not found in the database',
-}
